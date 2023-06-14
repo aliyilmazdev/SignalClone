951f91723b2ae98d4ffc75951750d842a4ffe065
Ali YILMAZ <ali@uskudar.dev>
2023-06-14 17:33:03 +0300

    Edited

--- Router.tsx
+++ Router.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import FirstScreen from "./src/screens/FirstScreen";
+import HomeScreen from "./src/screens/HomeScreen";
 import SecondScreen from "./src/screens/SecondScreen";
 const Tab = createBottomTabNavigator();
 
 export default function Tabs() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="FirstScreen" component={FirstScreen} />
+      <Tab.Screen name="HomeScreen" component={HomeScreen} />
       <Tab.Screen name="SecondScreen" component={SecondScreen} />
     </Tab.Navigator>
   );
